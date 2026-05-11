ab3fb3327ff1c26c890fe0a3082a8baffb6db977
poppychss <potterfc37@gmail.com>
2026-05-11 06:40:44 -0400

    add MongoDB connection

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:EWi6ACZ5tmGNNTlCRNNcqZcd@172.21.174.17:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
