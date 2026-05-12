b40568523cec9345719731b4743feaa637624279
poppychss <potterfc37@gmail.com>
2026-05-12 03:41:04 -0400

    add find doctor search and route

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from "./Components/Login/Login";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
-import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation"
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 // Function component for the main App
 function App() {
@@ -29,6 +30,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Sign_Up />} />
             <Route path="/instant-consultation" element={<InstantConsultation />} />
+            <Route path="/search/doctors" element={<FindDoctorSearch />} />
           </Routes>
         </BrowserRouter>
     </div>
