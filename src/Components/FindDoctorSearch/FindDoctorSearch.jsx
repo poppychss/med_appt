f8a2625d0864454a1cdf5ecd64e2719145f0e3f4
poppychss <potterfc37@gmail.com>
2026-05-12 03:58:02 -0400

    fix searchIcon on find doctor disappear

--- src/Components/FindDoctorSearch/FindDoctorSearch.jsx
+++ src/Components/FindDoctorSearch/FindDoctorSearch.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './FindDoctorSearch.css';
 import { useNavigate, Navigate } from 'react-router-dom';
-
+import searchIcon from "../../assets/search.svg";
 
 const initSpeciality = [
     'Dentist', 'Gynecologist/obstetrician', 'General Physician', 'Dermatologist', 'Ear-nose-throat (ent) Specialist', 'Homeopath', 'Ayurveda'
@@ -29,11 +29,13 @@ const FindDoctorSearch = () => {
 
                         <input type="text" className="search-doctor-input-box" placeholder="Search doctors, clinics, hospitals, etc." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />
                         
-                        <div className="findiconimg"><img className='findIcon' src={process.env.PUBLIC_URL + '/images/search.svg'} alt=""/></div>
+                        <div className="findiconimg">
+                            <img className="findIcon" src={searchIcon} alt="search" />
+                        </div>
                         <div className="search-doctor-input-results" hidden={doctorResultHidden}>
                             {
                                 specialities.map(speciality => <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
-                                    <span><img src={process.env.PUBLIC_URL + '/images/search.svg'} alt="" style={{height:"10px", width:"10px"}} width="12" /></span>
+                                    <span><img src={searchIcon} alt="" style={{height:"10px", width:"10px"}} width="12" /></span>
                                     <span>{speciality}</span>
                                     <span>SPECIALITY</span>
                                 </div>)
